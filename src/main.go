21a13ee51608b352baedb89191f075d425b99a3d
Luciano Barreira <luc.s.barreira@gmail.com>
2019-04-22 01:43:15 -0300

    Clean main.go

--- src/main.go
+++ src/main.go
@@ -1,34 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	// "fmt"
+	// "log"
 
-	"roboime-atlas/protos"
+	// sslProto "github.com/roboime/roboime-atlas/src/protos/ssl_proto"
 
-	"github.com/golang/protobuf/proto"
+	// "github.com/golang/protobuf/proto"
 )
 
 func main() {
-	proto.
-	test := &sslpkg.Test{
-		Label: proto.String("hello"),
-		Type:  proto.Int32(17),
-		Reps:  []int64{1, 2, 3},
-	}
-	data, err := proto.Marshal(test)
-	fmt.Println(data)
-	if err != nil {
-		log.Fatal("marshaling error: ", err)
-	}
-	newTest := &sslpkg.Test{}
-	err = proto.Unmarshal(data, newTest)
-	if err != nil {
-		log.Fatal("unmarshaling error: ", err)
-	}
-	// Now test and newTest contain the same data.
-	if test.GetLabel() != newTest.GetLabel() {
-		log.Fatalf("data mismatch %q != %q", test.GetLabel(), newTest.GetLabel())
-	}
-	// etc.
 }
