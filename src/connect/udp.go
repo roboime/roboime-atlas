06fdf41ee43315e22b3054565c9edb71476f7f0b
Luciano Barreira <meckmao@gmail.com>
2019-07-27 18:49:27 -0300

    Add TLS and some headers (to make it work with react client)

--- src/connect/udp.go
+++ src/connect/udp.go
@@ -2,14 +2,27 @@ package connect
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"os"
+	"strings"
 	"time"
 
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+
 	"github.com/golang/protobuf/proto"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"google.golang.org/grpc"
 
 	"github.com/roboime/roboime-atlas/src/protos/ssl"
+)
+
+const (
+	crt = "../localhost.crt"
+	key = "../localhost.key"
 )
 
 func ListenToVision() {
@@ -47,26 +60,44 @@ type roboIMEAtlas struct {
 }
 
 func (r *roboIMEAtlas) GetFrame(timestamp *ssl.Timestamp, stream ssl.RoboIMEAtlas_GetFrameServer) error {
+	if r.currentFrame == nil {
+		return nil
+	}
+	log.Println("geometry", r.currentFrame.GetGeometry())
 	return stream.Send(r.currentFrame)
 }
 
 func StartRoboIMEAtlasServer(atlas *roboIMEAtlas) {
-	lis, err := net.Listen("tcp", "127.0.0.1:10002")
-	if err != nil {
-		log.Fatalf("failed to listen %v", err)
-	}
-
 	grpcServer := grpc.NewServer()
 	ssl.RegisterRoboIMEAtlasServer(grpcServer, atlas)
+	logger := grpclog.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
 
-	err = grpcServer.Serve(lis)
-	if err != nil {
-		panic(err)
+	wrappedServer := grpcweb.WrapServer(grpcServer)
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		wrappedServer.ServeHTTP(resp, req)
 	}
+	h := allowCORS(http.HandlerFunc(handler))
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf(":9090"),
+		Handler: h,
+	}
+
+	logger.Infof("Starting server on port 9091 with tls")
+
+	if err := httpServer.ListenAndServeTLS(crt, key); err != nil {
+		logger.Fatalf("failed starting http2 server: %v", err)
+	}
+	// if err := httpServer.ListenAndServe(); err != nil {
+	// 	logger.Fatalf("failed starting http2 server: %v", err)
+	// }
 }
 
 func ClientTest() {
-	conn, err := grpc.Dial("127.0.0.1:10002", grpc.WithInsecure())
+	creds, err := credentials.NewClientTLSFromFile(crt, "")
+	if err != nil {
+		panic(err)
+	}
+	conn, err := grpc.Dial("https://127.0.0.1:9090", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		panic(err)
 	}
@@ -74,6 +105,7 @@ func ClientTest() {
 
 	client := ssl.NewRoboIMEAtlasClient(conn)
 
+	log.Println("Client test started")
 	for {
 		time.Sleep(17 * time.Millisecond)
 		stream, err := client.GetFrame(context.Background(), &ssl.Timestamp{})
@@ -89,4 +121,27 @@ func ClientTest() {
 			log.Println(pkt)
 		}
 	}
+}
+
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+// preflightHandler adds the necessary headers in order to serve
+// CORS from any origin using the methods "GET", "HEAD", "POST", "PUT", "DELETE"
+// We insist, don't do this without consideration in production systems.
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept", "Authorization", "x-grpc-web"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
 }
